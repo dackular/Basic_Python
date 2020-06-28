8115567d5cfd09c1dc75e7d6eee537d80c0dd577
DACKular <dackular@DACKulars-MBP.lan>
2020-06-28 23:35:27 +0700

    Add demo003.py

--- demo002.py
+++ demo002.py
@@ -1,4 +1,19 @@
 print("5"+"5")
 if 5>4:
     print("Yes")
+
+x = 10
+y = 5
+z = x + y
+print(z)
     
+age = int(input("Age: "))
+if age < 18:
+    print("Stop")
+
+
+age2 = input("Age2: ")
+if int(age2) < 18:
+    print("Stop")
+    
+
---
+++ demo003.py
@@ -0,0 +1,31 @@
+# Commant only one
+"""
+Comment many text
+"""
+
+text = "Hello"
+print(text[0])
+
+print(text[0:2])
+
+print(text[0:4])
+
+
+#customer1 = "John"
+#customer2 = "Robert"
+#customer3 = "Sun"
+customer_list = ["John","Robert","Sun"]
+customer_list.append("Hayabusa")
+customer_list.append("Naruto")
+customer_list.append("John")
+customer_list.append("Sun")
+print(customer_list)
+print(customer_list[0])
+print("Hello ", customer_list[0])
+print("Hello", customer_list[0:4])
+
+set_test = {"apple", "banana", "orange"}
+set_test.add("corn")
+set_test.add("apple")
+set_test.add("banana")
+print(set_test)
