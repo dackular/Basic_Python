d6f3969530b3c7654173840b9fd594ed676857a2
DACKular <dackular@DACKulars-MBP.lan>
2020-07-02 22:34:50 +0700

    Add demo

--- demo007-loop.py
+++ demo007-loop.py
@@ -5,3 +5,10 @@ index = 7
 print("{0} * {1} = {2}".format(5,index,5*index))
 
 print(f"{5} * {index} = {5*index}")
+
+for j in range(1,11):
+    print(f"{j}")
+
+for k in range(1,11):
+    print(f"5 * {k} = {5*k}")
+
---
+++ demo008-use-loop.py
@@ -0,0 +1,16 @@
+for i in range(1,11):
+    print(i)
+    print(2*i)
+
+
+
+for j in range(1,11,2):
+    print(j)
+
+
+for k in range(10,0,-1):
+    print(k)
+
+
+for l in range(10,0,-1):
+    print(l*l)
