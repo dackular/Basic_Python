3a053fd0d760e829595ab30e5ff85b51a9af5a3e
DACKular <dackular@DACKulars-MBP.lan>
2020-07-03 08:37:16 +0700

    add demo

---
+++ demo009-def.py
@@ -0,0 +1,8 @@
+print("Hello World (Not Define)")
+
+def print_hello_world_twice():
+    print("Hello World")
+    print("Hello World")
+
+
+print_hello_world_twice()
---
+++ demo010-def-define.py
@@ -0,0 +1,20 @@
+def print_hello_world(no_of_time):
+    for i in range(1,no_of_time+1):
+        print("Hello World")
+
+
+print_hello_world(3)
+
+
+def print_number(n):
+    for i in range(1,n+1):
+        print(i)
+
+print_number(3)
+
+
+def print_square_number(z):
+    for y in range(1,z+1):
+        print(y*y)
+
+print_square_number(5)
