6660c5d56e1efb1b36871b5704dd3212b9681a14
chartiporn@ait.co.th <chartiporn@ait.co.th>
2020-07-25 11:04:12 +0700

    add lecture

---
+++ sequence1-tuple.py
@@ -0,0 +1,14 @@
+a = ('TEST1','TEST2','TEST3')
+print(a)
+print(type(a))
+
+print('====================')
+
+print(a[2])
+
+print('====================')
+
+print(a[0])
+
+print('====================')
+
---
+++ sequence2-list.py
@@ -0,0 +1,8 @@
+a = ['TEST1','TEST2','TEST3']
+print(type(a))
+print('====================')
+print(a[1])
+print('====================')
+print(a[0])
+print('====================')
+print(a[2])
