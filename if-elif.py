8e0353afe3725f1a157062a9f7d8039ba5578d23
chartiporn@ait.co.th <chartiporn@ait.co.th>
2020-07-27 08:13:26 +0700

    add lecture

---
+++ if-elif.py
@@ -0,0 +1,11 @@
+a = int(input("Please enter number : "))
+if a == 10:
+    print(a)
+elif a == 20:
+    print(a)
+elif a == 30:
+    print(a)
+elif a >= 100:
+    print("WoW a > 100")
+else:
+    print('Not OK')
---
+++ ifelse1.py
@@ -0,0 +1,5 @@
+a = int(input("Please Enter Value : "))
+if a <= 500:  # a >= 500, a!= 500
+    print('Yes')
+else:
+    print("No")
