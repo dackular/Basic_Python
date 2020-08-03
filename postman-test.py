6fbd79d0ae665b02ec092d2d18df72958d997f5e
chartiporn@ait.co.th <chartiporn@ait.co.th>
2020-08-03 21:55:11 +0700

    add postman

---
+++ postman-test.py
@@ -0,0 +1,17 @@
+import requests
+
+url = "https://deckofcardsapi.com/api/deck/87nfwro26sjd/draw/"
+
+querystring = {"count":"6"}
+
+headers = {
+    'cache-control': "no-cache",
+    'postman-token': "5d0fe71a-1337-93ca-b4d0-8ba5cb9b32d8"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+deck = response.json()
+deck_id = deck['deck_id']
+print(deck_id)
+#print(response.text)
