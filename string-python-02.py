247999b125bab20c5b17cb531cdb81672f925033
chartiporn@ait.co.th <chartiporn@ait.co.th>
2020-08-20 13:06:46 +0700

    update

--- string-python-02.py
+++ string-python-02.py
@@ -1,3 +1,26 @@
 age = 34
 age_str = str(age)
 print("You are " + age_str)
+print("-------------------")
+print(f"You are {age}")
+print("-------------------")
+
+
+name = "Jose"
+greeting = f"How are you, {name}?"
+print(greeting)
+print("-------------------")
+name = "Bob"
+print(greeting)
+
+print("-------------------")
+
+
+name = "Abc"
+final_greeting = "How are you, {}?"
+abc_greeting = final_greeting.format(name)
+print(abc_greeting)
+
+name = "Def"
+def_greeting = final_greeting.format(name)
+print(def_greeting)
