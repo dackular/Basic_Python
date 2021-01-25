c9c19895af2eccf1a05048ed85f558f002179582
dackular <dackular@hotmail.com>
2021-01-25 13:02:21 +0700

    update linenotify

--- linenotify.py
+++ linenotify.py
@@ -2,13 +2,9 @@ import requests
 
 url = 'https://notify-api.line.me/api/notify'
 token = 'DB0yEn8zezvk2MxmRXPhamHd4ZfbdFeP91pHfeXMa02'
-headers = {
-            'content-type':
-            'application/x-www-form-urlencoded',
-            'Authorization':'Bearer '+token
-           }
+headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
 
 while True:
-    msg = input("Enter your name:")
-    r = requests.post(url, headers=headers , data = {'message':msg})
-    print(r.text)
+	msg = input('Enter Message : ')
+	r = requests.post(url, headers=headers, data={'message':msg})
+	print(r.text)
